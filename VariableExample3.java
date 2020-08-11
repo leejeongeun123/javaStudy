66f75780a67549952ec2884fb0889197bb05a3cc
leejeongeun <wjddms5700@naver.com>
2020-08-11 10:49:18 +0900

    ignore add

---
+++ VariableExample3.java
@@ -0,0 +1,11 @@
+public class VariableExample3 {
+	public static void main(String[] args) {
+		byte b1 = -128;
+		byte b2 = 127;
+		
+		System.out.println(b2++); // b2 = b2 + 1 ; 
+		System.out.println(b2++); // b2 = b2 + 1 ; 
+		System.out.println(b2++); // b2 = b2 + 1 ; 
+	}
+	
+}
